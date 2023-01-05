a8673e4419933f94e9ae1ca764bd1e33115c8834
NKIyuki <u.y0701@icloud.com>
2023-01-05 12:49:25 +0000

    END

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,16 +4,17 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-       redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to book_path(@book.id)
     else
       render :new
     end
   end
 
   def index
-    @book = Book.all
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
